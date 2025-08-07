4a13a3a8085bd5a959c7b6473c1859331660de12
Marnie O'Boyle <marnieoboyle.isl@gmail.com>
2025-08-07 23:21:57 +0000

    It is not intuitive for users to find how to delete projects. Update the

--- src/components/project-switcher.tsx
+++ src/components/project-switcher.tsx
@@ -112,7 +112,7 @@ export function ProjectSwitcher({
         <DialogTrigger asChild>
           <Button disabled={!isReady}>
             <Save className="mr-2 h-4 w-4" />
-            Save Project
+            Save / Manage Project
           </Button>
         </DialogTrigger>
         <DialogContent>
